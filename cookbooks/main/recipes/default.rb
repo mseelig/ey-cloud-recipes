3b6d1113a2c9590eccfe1e4fe0e175ef13a16374
Mark Seelig <mseelig@NFTW0AWPC02KN17G.local>
2013-06-06 16:34:23 -0400

    enable delayed job recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+include_recipe "delayed_job"
+
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # include_recipe "sphinx"
 
